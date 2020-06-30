3d3142a12187a4df5a15f909d46b049dbf954648
Nicola Taddei <nicola.taddei.94@gmail.com>
2020-06-30 15:07:27 +0200

    fix(service): service is upstream

--- src/microservices/user/src/index.ts
+++ src/microservices/user/src/index.ts
@@ -21,7 +21,7 @@ import { CONNECTIONS } from './connection';
 import * as polly from 'polly-js';
 
 const routingControllersOptions = {
-  routePrefix: '/user',
+  // routePrefix: '/user',
   controllers: CONTROLLERS,
   middlewares: MIDDLEWARES,
   defaults: {
