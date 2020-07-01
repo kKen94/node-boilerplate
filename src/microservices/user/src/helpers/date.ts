b6fa0e93a5600c43ca78685ce4cde051ca1283f2
Nicola <nicola.taddei.94@gmail.com>
2020-07-01 13:06:17 +0200

    fix(normalize date): return null if there is not value

--- src/microservices/user/src/helpers/date.ts
+++ src/microservices/user/src/helpers/date.ts
@@ -1,4 +1,7 @@
 export const normalizeDbDate = (dateFromDb: Date): Date => {
+  if (!dateFromDb) {
+    return null;
+  }
   const normalizedTimeZone =
     dateFromDb.getTime() - dateFromDb.getTimezoneOffset() * 60000;
   return new Date(normalizedTimeZone);
